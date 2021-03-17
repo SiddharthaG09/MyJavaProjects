160aacf662829e2abe6c81d5468c574b67ab7f61
SiddharthaG09 <78454757+SiddharthaG09@users.noreply.github.com>
2021-03-17 17:18:38 -0500

    PhoneDialer
    A graphical interface to dial a phone number

--- PhoneDialer/src/PhoneDialer.java
+++ PhoneDialer/src/PhoneDialer.java
@@ -2,7 +2,7 @@ import java.awt.*;
 import javax.swing.*;
 import java.awt.event.*;
 import javax.swing.border.*;
-//© Siddhartha inc. ©2021
+// © Siddhartha inc. ©2021 
 public class PhoneDialer implements ActionListener
 {
 	JButton zero;
